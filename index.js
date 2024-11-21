c180a8dd484427ecfa3423ec309605c35456558d
MihailPetkov96 <mpetkov228@gmail.com>
2024-11-21 20:00:36 +0200

    Complete exercise 8.7 Updating the birth year of an author

--- index.js
+++ index.js
@@ -113,6 +113,10 @@ const typeDefs = `
       published: Int!
       genres: [String!]!
     ): Book!
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `;
 
@@ -159,6 +163,16 @@ const resolvers = {
 
       books = books.concat(newBook);
       return newBook;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a);
+      return updatedAuthor;
     }
   }
 };
